a9c62e27514ed0b4aeeda9d55361dd9c184e5557
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-01 22:15:15 -0500

    feature(nvim): modified lazyvim

---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      mode = { "n" },
+      { "<leader>m", group = "Mark" },
+      { "<leader>md", group = "Delete mark" },
+    },
+  },
+}
