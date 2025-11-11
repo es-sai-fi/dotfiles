c26a545dc4d9955724b64039d7a7710ccdd5e1f2
es-sai-fi <96452903+es-sai-fi@users.noreply.github.com>
2025-11-11 16:50:34 -0500

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,39 +7,3 @@ vim.keymap.set("n", "<leader>fh", function()
     cwd = vim.fn.expand("~"),
   })
 end, { desc = "Find Files (Home Dir)" })
-
--- Marks
-vim.keymap.set("n", "<leader>ma", function()
-  vim.cmd("argadd %")
-  vim.cmd("argdedup")
-  vim.notify("Added mark")
-end, { desc = "Add mark" })
-
-for i = 1, 9 do
-  vim.keymap.set("n", "<leader>md" .. i, function()
-    local file = vim.fn.argv(i - 1)
-    vim.cmd("argdelete " .. vim.fn.fnameescape(file))
-    vim.notify("Removed marked file #" .. i)
-  end, { desc = "Delete mark #" .. i })
-end
-
-vim.keymap.set("n", "<leader>ms", function()
-  local args = vim.fn.argv()
-  if #args == 0 then
-    vim.notify("No marks yet", vim.log.levels.INFO)
-    return
-  end
-
-  local list = {}
-  for i, file in ipairs(args) do
-    table.insert(list, string.format("%d. %s", i, file))
-  end
-
-  vim.notify(table.concat(list, "\n"), vim.log.levels.INFO, { title = "Marked files" })
-end, { desc = "Show marks" })
-
-for i = 1, 9 do
-  vim.keymap.set("n", "<leader>" .. i, function()
-    vim.cmd("silent! " .. i .. "argument")
-  end, { desc = "which_key_ignore" })
-end
